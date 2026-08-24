b40447cd6d25eaf62e39dff02ae02901271fc63f
longx1643-crypto <longx1643@gmail.com>
2026-08-24 23:44:31 +0700

    Update Java and Kotlin compatibility to version 17
    Set Java and Kotlin compatibility to version 17.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,12 @@ plugins {
     id("org.jetbrains.kotlin.plugin.compose")
 }
 
-android {
+android {compileOptions {
+    sourceCompatibility = JavaVersion.VERSION_17
+    targetCompatibility = JavaVersion.VERSION_17
+}kotlinOptions {
+    jvmTarget = "17"
+}
     namespace = "com.moneysaver.app"
     compileSdk = 35
 
